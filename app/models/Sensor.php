db59b67b945895db87a2a83decc09ae640d2d025
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-11 14:53:24 -0400

    added ;

--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -21,7 +21,7 @@ class Sensor
     // 1. Connect to the database
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     // 2. Prepare the query
-    $sql = 'SELECT * FROM sensor';
+    $sql = 'SELECT * FROM sensor;';
     $statement = $db->prepare($sql);
     // 3. Run the query
     $success = $statement->execute();
