f6707c4bc57647d2f26db1255043cd51f966f96c
Sam Stowell <smstowel@indiana.edu>
2018-10-11 17:52:56 -0400

    added create function for post

--- app/models/Client.php
+++ app/models/Client.php
@@ -18,6 +18,22 @@ class Client
     $this->headquarters = $row['headquarters'];
   }
 
+  public function create() {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    $sql = 'INSERT INTO client (clientId,clientName, clientDescription, gicsSector, gicsSubIndustry, headquarters)
+            VALUES (?, ?, ?, ?, ?, ?)';
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->clientId,
+      $this->clientName,
+      $this->clientDescription,
+      $this->gicsSector,
+      $this->gicsSubIndustry,
+      $this->headquarters
+    ]);
+    $this->clientId = $db->lastInsertId();
+  }
+
 
   public static function fetchAll() {
     // 1. Connect to the database
