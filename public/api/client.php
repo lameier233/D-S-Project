efca09a85f83f64fc557247f5f18b7f2e2e11458
Sam Stowell <smstowel@indiana.edu>
2018-10-11 15:16:10 -0400

    client updated

--- public/api/client.php
+++ public/api/client.php
@@ -1,1 +1,13 @@
 <?php
+require '../../app/common.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require 'clientPost.php';
+  exit;
+
+}
+
+$sensorArr = Client::fetchAll();
+$json = json_encode($clientArr, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
