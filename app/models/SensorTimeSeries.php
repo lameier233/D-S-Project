ab2e80b614d3c01f57857a034e82bcc0e8ab5c73
Sam Stowell <smstowel@indiana.edu>
2018-10-11 16:47:54 -0400

    sensorTimeSeries

--- app/models/SensorTimeSeries.php
+++ app/models/SensorTimeSeries.php
@@ -1,1 +1,49 @@
 <?php
+
+class SensorTimeSeries
+{
+  public $sensorDeployedId;
+  public $dataCollectionDate;
+  public $output;
+  public $heatrate;
+  public $compressorEfficiency;
+  public $availability;
+  public $reliability;
+  public $fireHours;
+  public $trips;
+  public $starts;
+
+  public function __construct($row) {
+    $this->sensorDeployedId = isset($row['sensorDeployedId']) ? intval($row['sensorDeployedId']) : null;
+    $this->dataCollectionDate = $row['dataCollectionDate'];
+    $this->output = $row['output'];
+    $this->heatrate = $row['heatrate'];
+    $this->compressorEfficiency = $row['compressorEfficiency'];
+    $this->availability = $row['availability'];
+    $this->reliability = $row['reliability'];
+    $this->fireHours = $row['fireHours'];
+    $this->starts = $row['starts'];
+  }
+
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM sensorTimeSeries';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+      // 4.a. For each row, make a new work object
+      $newSensorTimeSeries =  new SensorTimeSeries($row);
+      array_push($arr, $newSensorTimeSeries);
+    }
+    // 4.b. return the array of work objects
+    return $arr;
+  }
+
+}
---
+++ public/api/sensorTimeSeries.php
@@ -0,0 +1,13 @@
+<?php
+require '../../app/common.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require 'sensorTimeSeriesPost.php';
+  exit;
+
+}
+
+$clientArr = SensorTimeSeries::fetchAll();
+$json = json_encode($sensorTimeSeriesArr, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
