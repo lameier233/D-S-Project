98dd3af672aed7aa0db9e11fc186f4d885b9106e
Sam Stowell <smstowel@indiana.edu>
2018-10-11 14:30:09 -0400

    environment default, sensor data implemented

--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -1,1 +1,41 @@
 <?php
+
+class Sensor
+{
+  public $sensorId;
+  public $sensorName;
+  public $sensorDescription;
+  public $manufacturer;
+  public $totalLifeExpectancyHours;
+
+  public function __construct($row) {
+    $this->id = isset($row['id']) ? intval($row['sensorId']) : null;
+    $this->sensorName = $row['sensorName'];
+    $this->sensorDescription = $row['sensorDescription'];
+    $this->manufacturer = $row['manufacturer'];
+    $this->totalLifeExpectancyHours = $row['totalLifeExpectancyHours'];
+  }
+
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Sensor';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+      // 4.a. For each row, make a new work object
+      $newSensor =  new Sensor($row);
+
+      array_push($arr, $newSensor);
+    }
+    // 4.b. return the array of work objects
+    return $arr;
+  }
+
+}
--- public/api/sensor.php
+++ public/api/sensor.php
@@ -1,1 +1,13 @@
 <?php
+require '../../app/common.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require 'sensorPost.php';
+  exit;
+
+}
+
+$sensorArr = Sensor::fetchAll();
+$json = json_encode($sensorArr, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
