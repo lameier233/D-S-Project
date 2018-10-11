23634b0d1d45e4a608fddbcb50ba3989d36c0877
Connor <cshudak@iu.edu>
2018-10-11 16:56:11 -0400

    Site api

--- app/models/Site.php
+++ app/models/Site.php
@@ -1,1 +1,56 @@
 <?php
+
+class Site
+{
+  public $siteId;
+  public $clientId;
+  public $siteName;
+  public $siteDescription;
+  public $primaryContact;
+  public $capacity;
+  public $commercialDate;
+  public $addrLine1;
+  public $addrLine2;
+  public $addrCity;
+  public $addrState;
+  public $addrZip;
+  public $addrCountry;
+
+  public function __construct($row) {
+    $this->siteId = isset($row['siteId']) ? intval($row['siteId']) : null;
+    $this->clientId = $row['clientId'];
+    $this->siteName = $row['siteName'];
+    $this->siteDescription = $row['siteDescription'];
+    $this->primaryContact = $row['primaryContact'];
+    $this->capacity = $row['capacity'];
+    $this->commercialDate = $row['commercialDate'];
+    $this->addrLine1 = $row['addrLine2'];
+    $this->addrLine2 = $row['addrLine2'];
+    $this->addrCity = $row['addrCity'];
+    $this->addrState = $row['addrState'];
+    $this->addrZip = $row['addrZip'];
+    $this->addrCountry = $row['addrCountry'];
+  }
+
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM client';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+      // 4.a. For each row, make a new work object
+      $newSite =  new Site($row);
+      array_push($arr, $newSite);
+    }
+    // 4.b. return the array of work objects
+    return $arr;
+  }
+
+}
