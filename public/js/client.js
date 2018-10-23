69747ed4fb64831ff86f6ca4171e6825f7683c8f
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-23 09:44:45 -0400

    created client json file

---
+++ public/js/client.js
@@ -0,0 +1,18 @@
+var clientInfor = new Vue({
+  el: '#clientMain',
+  data: {
+    client: [ ]
+  },
+
+  created () {
+    // TODO: Fetch task-specific data
+    // fetch('api/task?id=4')
+    fetch('../api/client.php')
+    .then( response => response.json() )
+    .then( json => {clientInfor.client = json} )
+    .catch( err => {
+      console.log('CLIENT FETCH ERROR:');
+      console.log(err);
+    })
+  }
+})
