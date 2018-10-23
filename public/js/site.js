2e373aa59ef9ba972d6921709b92dcf9d4135490
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-23 09:21:16 -0400

    Created site json file

--- public/js/site.js
+++ public/js/site.js
@@ -4,48 +4,14 @@ var siteInfor = new Vue({
     site: [ ]
   },
 
-  methods: {
-    handleCommentPost(e) {
-      e.preventDefault();
-
-      const s = JSON.stringify (this.commentForm);
-      console.log(s);
-      // POST to remote server
-      fetch('api/comment.php', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: s // body data type must match "Content-Type" header
-      })
-      .then( function(response) {
-        console.log(response);
-        return response.json();
-      })
-      .then( json => {this.comment.push(json)})
-      .catch( err => {
-        console.error('COMMENT POST ERROR:');
-        console.error(err);
-      })
-
-      this.commentForm = this.getEmptyCommentForm();
-    },
-
-    getEmptyCommentForm(){
-      return{
-        comment:null
-      }
-    },
-  },
-
   created () {
     // TODO: Fetch task-specific data
     // fetch('api/task?id=4')
-    fetch('../api/comment.php')
+    fetch('../api/site.php')
     .then( response => response.json() )
-    .then( json => {commentsApp.comment = json} )
+    .then( json => {siteInfor.site = json} )
     .catch( err => {
-      console.log('COMMENT FETCH ERROR:');
+      console.log('SITE FETCH ERROR:');
       console.log(err);
     })
   }
