a340d977c3829883c88f29c26c68a49037d116c7
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-24 13:35:41 -0400

    Hopefully notes works

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,13 +1,57 @@
 var notesInfor = new Vue({
   el: '#notesMain',
   data: {
-    notes: []
+    notes: [ ],
+    notesForm: {}
   },
 
+  methods: {
+  handleNotesPost(e) {
+    e.preventDefault();
+
+    const s = JSON.stringify (this.notesForm);
+    console.log(s);
+    // POST to remote server
+    fetch('api/notes.php, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+          "Content-Type": "application/json; charset=utf-8"
+      },
+      body: s // body data type must match "Content-Type" header
+    })
+    .then( function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then( json => {this.notes.push(json)})
+    .catch( err => {
+      console.error('Note Post Error:');
+      console.error(err);
+    })
+
+    this.notesForm = this.getEmptyNotesForm();
+  },
+
+  getEmptyNotesForm(){
+    return{
+      notes:null
+    }
+  },
+},
+
   created () {
-  // Do data fetch
-const url = new URL(window.location.href);
-const client.id = url.searchParams.get('client.id');
-console.log('Notes: '+ client.id);
-this.client.id = client.id;
+    // Do data fetch
+  const url = new URL(window.location.href);
+  const clientId = url.searchParams.get('clientId');
+  console.log('Notes: '+ clientId);
+  this.clientId = clientId;
+
+  fetch('../api/notes.php?clientId='+clientId)
+    .then( response => response.json() )
+    .then( json => {commentsApp.comment = json} )
+    .catch( err => {
+      console.log('COMMENT FETCH ERROR:');
+      console.log(err);
+    })
+
 }
