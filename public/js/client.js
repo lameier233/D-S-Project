a3a78b930a2262c1aad5dbe7bb194f5940ec0898
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-24 09:37:28 -0400

    added notes section for client page

--- public/js/client.js
+++ public/js/client.js
@@ -1,44 +1,9 @@
 var clientInfor = new Vue({
   el: '#clientMain',
   data: {
-    client: [ ],
-    noteForm: {}
+    client: [ ]
   },
-
-  methods: {
-      handleNotePost(e) {
-        e.preventDefault();
-
-        const s = JSON.stringify (this.noteForm);
-        console.log(s);
-        // POST to remote server
-        fetch('api/clientPost.php', {
-          method: "POST", // *GET, POST, PUT, DELETE, etc.
-          headers: {
-              "Content-Type": "application/json; charset=utf-8"
-          },
-          body: s // body data type must match "Content-Type" header
-        })
-        .then( function(response) {
-          console.log(response);
-          return response.json();
-        })
-        .then( json => {this.note.push(json)})
-        .catch( err => {
-          console.error('NOTE POST ERROR:');
-          console.error(err);
-        })
-
-        this.noteForm = this.getEmptyNoteForm();
-      },
-
-      getEmptyNoteForm(){
-        return{
-          note:null
-        }
-      },
-    },
-
+  
   created () {
     // TODO: Fetch task-specific data
     // fetch('api/task?id=4')
