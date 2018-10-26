2d770b76fd3e35a7b53ada284b751371d6861152
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-26 13:02:20 -0400

    eg qknqektiqhlt

--- public/js/site.js
+++ public/js/site.js
@@ -1,8 +1,44 @@
 var siteInfor = new Vue({
   el: '#siteMain',
   data: {
-    site: [ ]
+    site: [ ],
+    siteForm: {}
   },
+
+  methods: {
+  handleNotesPost(e) {
+    e.preventDefault();
+    var tempQ = window.location.href.indexOf("=",0);
+    this.siteForm.clientId = window.location.href.slice(tempQ + 1,tempQ + 2);
+    const s = JSON.stringify (this.siteForm);
+    // POST to remote server
+    fetch('api/site.php', {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+          "Content-Type": "application/json; charset=utf-8"
+      },
+      body: s // body data type must match "Content-Type" header
+    })
+    .then( function(response) {
+      //console.log(response);
+      return response.json();
+    })
+    .then( json => {siteInfor.notes.push(json)})
+    .catch( err => {
+      console.error('Site Post Error:');
+      console.error(err);
+    })
+
+    this.siteForm = this.getEmptySiteForm();
+  },
+
+  getEmptySiteForm(){
+    return{
+      clientId:null,
+      site:null
+    }
+  },
+},
 
   created () {
     const url = new URL(window.location.href);
