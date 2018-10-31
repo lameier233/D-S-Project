d82dc32ce6f3ff202c261cb3d07555e0b3751f9c
jberdovi <jberdovi@iu.edu>
2018-10-30 23:43:57 -0400

    m

--- public/api/sensor.php
+++ public/api/sensor.php
@@ -10,5 +10,5 @@ $sensorId = intval($_GET['sensorId'] ?? 0);
 $sensorArr = Sensor::fetchAll($sensorId);
 $json = json_encode($sensorArr, JSON_PRETTY_PRINT);
 
-header('Content-Type: application/jso$clientId = intval($_GET['clientId'] ?? 0);n');
+header('Content-Type: application/json');
 echo $json;
