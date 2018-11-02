96f8774c0c868dcfc4d4a965784bfb436e67428f
Lana La <lana@iu.edu>
2018-11-01 22:45:48 -0400

    Update public/js/sensorTimeSeries.js

--- public/js/sensorTimeSeries.js
+++ public/js/sensorTimeSeries.js
@@ -64,7 +64,7 @@ var sensorTimeSeriesInfor = new Vue({
         buildHeatChart: function() {
             var data = [];
             for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
-                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].heatRate)]);
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].heatrate)]);
             }
             Highcharts.chart('heatChart', {
                 chart: {
@@ -174,9 +174,292 @@ var sensorTimeSeriesInfor = new Vue({
                     data: data
                 }]
             });
-        }
-
-
+        },
+        buildAvailabilityChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].availability)]);
+            }
+            Highcharts.chart('availabilityChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'Availability from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Availability'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'Availability',
+                    data: data
+                }]
+            });
+        },
+        buildReliabilityChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].reliability)]);
+            }
+            Highcharts.chart('reliabilityChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'CReliability from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Reliability'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'Reliability',
+                    data: data
+                }]
+            });
+        },
+        buildFiredHoursChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].firedHours)]);
+            }
+            Highcharts.chart('firedChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'Fired from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fired'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'Fired',
+                    data: data
+                }]
+            });
+        },
+        buildTripsChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].trips)]);
+            }
+            Highcharts.chart('tripsChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'Trips from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Trips'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'Trips',
+                    data: data
+                }]
+            });
+        },
+        buildStartsChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].starts)]);
+            }
+            Highcharts.chart('startsChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'Starts Rate from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Starts'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'Starts',
+                    data: data
+                }]
+            });
+        },
     },
     created: function () {
         const url = new URL(window.location.href);
@@ -188,6 +471,11 @@ var sensorTimeSeriesInfor = new Vue({
                 sensorTimeSeriesInfor.buildOutputChart();
                 sensorTimeSeriesInfor.buildHeatChart();
                 sensorTimeSeriesInfor.buildCompressorChart();
+                sensorTimeSeriesInfor.buildAvailabilityChart();
+                sensorTimeSeriesInfor.buildReliabilityChart();
+                sensorTimeSeriesInfor.buildFiredHoursChart();
+                sensorTimeSeriesInfor.buildTripsChart();
+                sensorTimeSeriesInfor.buildStartsChart();
             })
             .catch( err => {
             console.log('Sensor Time Series FETCH ERROR:');
