4849a54c1acf8247946e09020daf02795de32831
Lana La <lana@iu.edu>
2018-11-01 22:31:01 -0400

    Update public/js/sensorTimeSeries.js

--- public/js/sensorTimeSeries.js
+++ public/js/sensorTimeSeries.js
@@ -60,7 +60,123 @@ var sensorTimeSeriesInfor = new Vue({
                     data: data
                 }]
             });
+        },
+        buildHeatChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].heatRate)]);
+            }
+            Highcharts.chart('heatChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'Heat Rate from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Heat Rate'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'Heat Rate',
+                    data: data
+                }]
+            });
+        },
+        buildCompressorChart: function() {
+            var data = [];
+            for(var i = 0; i < sensorTimeSeriesInfor.sensorTimeSeries.length; i++) {
+                data.push([Date.parse(sensorTimeSeriesInfor.sensorTimeSeries[i].dataCollectedDate), parseInt(sensorTimeSeriesInfor.sensorTimeSeries[i].compressorEfficiency)]);
+            }
+            Highcharts.chart('compressorChart', {
+                chart: {
+                    zoomType: 'x'
+                },
+                title: {
+                    text: 'Compressor Efficiency from January to June'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Compressor Efficiency'
+                    },
+                    min: 0
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'CompressorEfficiency',
+                    data: data
+                }]
+            });
         }
+
+
     },
     created: function () {
         const url = new URL(window.location.href);
@@ -70,6 +186,8 @@ var sensorTimeSeriesInfor = new Vue({
             .then( function(json) {
                 sensorTimeSeriesInfor.sensorTimeSeries = json;
                 sensorTimeSeriesInfor.buildOutputChart();
+                sensorTimeSeriesInfor.buildHeatChart();
+                sensorTimeSeriesInfor.buildCompressorChart();
             })
             .catch( err => {
             console.log('Sensor Time Series FETCH ERROR:');
